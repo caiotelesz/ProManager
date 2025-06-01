db84980a61db38d31c8898e4e97d74e755735bf5
caiotelesz <caioteles1267@gmail.com>
2025-06-01 15:58:57 -0300

    feat: add validation constraints and date consistency check to 'SaveProjectDataDTO'

--- src/main/java/com/caio/project_management/infrastructure/dto/SaveProjectDataDTO.java
+++ src/main/java/com/caio/project_management/infrastructure/dto/SaveProjectDataDTO.java
@@ -1,5 +1,6 @@
 package com.caio.project_management.infrastructure.dto;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -9,19 +10,25 @@ import java.time.LocalDate;
 @Data
 public class SaveProjectDataDTO {
 
-    @NotNull
-    @Size(min = 1, max = 36)
+    @NotNull(message = "Name cannot be empty")
+    @Size(min = 1, max = 36, message = "Invalid name")
     private final String name;
 
-    @NotNull
-    @Size(min = 1, max = 150)
+    @NotNull(message = "Description cannot be empty")
+    @Size(min = 1, max = 150, message = "Invalid description")
     private final String description;
 
-    @NotNull
+    @NotNull(message = "Initial date cannot be empty")
     private final LocalDate initialDate;
 
-    @NotNull
+    @NotNull(message = "Final date cannot be empty")
     private final LocalDate finalDate;
 
     private final String status;
+
+    @AssertTrue(message = "Dates are not consistent")
+    @SuppressWarnings("unused")
+    public boolean isInitialDateBeforeFinalDate() {
+        return initialDate.isBefore(finalDate);
+    }
 }
