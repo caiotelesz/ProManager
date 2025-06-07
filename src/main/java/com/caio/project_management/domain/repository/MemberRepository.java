1643a3bde2ccaffd149c8eeb9d58dc559bba21e4
caiotelesz <caioteles1267@gmail.com>
2025-06-07 10:39:53 -0300

    feat: add Member repository

---
+++ src/main/java/com/caio/project_management/domain/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.caio.project_management.domain.repository;
+
+import com.caio.project_management.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+    Optional<Member> findByIdAndDeleted(String id, boolean deleted);
+    Optional<Member> findByEmailAndDeleted(String email, boolean deleted);
+}
